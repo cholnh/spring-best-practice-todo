4d2647674f7f33079239e807ace79f93a91271ca
Nzzi <cholnh1@naver.com>
2021-04-20 04:26:48 +0900

    add crud 컨트롤러 with hateoas with cqrs -> command 부분은 리턴값을 http 상태값만 반환

--- src/main/java/com/nzzi/guide/todo/domain/todo/api/TodoApi.java
+++ src/main/java/com/nzzi/guide/todo/domain/todo/api/TodoApi.java
@@ -1,6 +1,9 @@
 package com.nzzi.guide.todo.domain.todo.api;
 
-import com.nzzi.guide.todo.domain.todo.service.TodoFindService;
+import com.nzzi.guide.todo.domain.todo.dto.TodoRequest;
+import com.nzzi.guide.todo.domain.todo.model.Todo;
+import com.nzzi.guide.todo.domain.todo.service.TodoCommandService;
+import com.nzzi.guide.todo.domain.todo.service.TodoQueryService;
 import com.nzzi.guide.todo.domain.todo.dto.TodoPredicate;
 import com.nzzi.guide.todo.domain.todo.dto.TodoResponse;
 import lombok.AllArgsConstructor;
@@ -9,8 +12,13 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -20,13 +28,15 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @AllArgsConstructor
 public class TodoApi {
 
-    private TodoFindService todoFindService;
+    private TodoQueryService todoQueryService;
+    private TodoCommandService todoCommandService;
 
     @GetMapping("/{todoId}")
-    public ResponseEntity<?> findOne(@PathVariable(value = "todoId", required = true) Long todoId) {
-
+    public ResponseEntity<?> findOne(
+            @PathVariable(value = "todoId") Long todoId
+    ) {
         EntityModel<TodoResponse> resources = EntityModel.of(
-            todoFindService.findTodo(todoId));
+            todoQueryService.findTodo(todoId));
 
         resources.add(linkTo(methodOn(TodoApi.class).findOne(todoId)).withSelfRel());
         return ResponseEntity.ok(resources);
@@ -38,10 +48,46 @@ public class TodoApi {
             @PageableDefault(sort = {"idx"}, direction = Sort.Direction.DESC, page = 0, size = 10) Pageable pageable
     ) {
         CollectionModel<TodoResponse> resources = CollectionModel.of(query == null
-            ? todoFindService.findTodos(pageable)
-            : todoFindService.searchByContents(TodoPredicate.of(query), pageable));
+            ? todoQueryService.findTodos(pageable)
+            : todoQueryService.searchByContents(TodoPredicate.of(query), pageable));
 
         resources.add(linkTo(methodOn(TodoApi.class).findGroup(query, pageable)).withSelfRel());
         return ResponseEntity.ok(resources);
+    }
+
+    @PostMapping
+    public ResponseEntity create(
+            @Valid @RequestBody TodoRequest request
+    ) {
+        Todo created = todoCommandService.create(request);
+        Link link = linkTo(methodOn(TodoApi.class).findOne(created.getIdx()))
+                .withSelfRel();
+        return ResponseEntity
+                .created(link.toUri())
+                .build();
+    }
+
+    @PutMapping("/{todoId}")
+    public ResponseEntity update(
+            @PathVariable(value = "todoId") Long todoId,
+            @RequestBody TodoRequest request
+    ) {
+        Todo updated = todoCommandService.update(todoId, request);
+        Link link = linkTo(methodOn(TodoApi.class).findOne(updated.getIdx()))
+                .withSelfRel();
+        return ResponseEntity
+                .accepted()
+                .location(link.toUri())
+                .build();
+    }
+
+    @DeleteMapping("/{todoId}")
+    public ResponseEntity delete(
+            @PathVariable(value = "todoId") Long todoId
+    ) {
+        todoCommandService.delete(todoId);
+        return ResponseEntity
+                .noContent()
+                .build();
     }
 }
