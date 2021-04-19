3c12930c66bd74a97d02ee4bf8d8e5a1d062d873
Nzzi <cholnh1@naver.com>
2021-04-20 04:27:15 +0900

    add query service

---
+++ src/main/java/com/nzzi/guide/todo/domain/todo/service/TodoQueryService.java
@@ -0,0 +1,12 @@
+package com.nzzi.guide.todo.domain.todo.service;
+
+import com.nzzi.guide.todo.domain.todo.dto.TodoPredicate;
+import com.nzzi.guide.todo.domain.todo.dto.TodoResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface TodoQueryService {
+    TodoResponse findTodo(Long id);
+    Page<TodoResponse> findTodos(Pageable pageable);
+    Page<TodoResponse> searchByContents(TodoPredicate predicate, Pageable pageable);
+}
--- src/main/java/com/nzzi/guide/todo/domain/todo/service/impl/TodoQueryServiceImpl.java
+++ src/main/java/com/nzzi/guide/todo/domain/todo/service/impl/TodoQueryServiceImpl.java
@@ -1,10 +1,11 @@
-package com.nzzi.guide.todo.domain.todo.service;
+package com.nzzi.guide.todo.domain.todo.service.impl;
 
+import com.nzzi.guide.todo.domain.todo.dao.jpa.TodoRepository;
 import com.nzzi.guide.todo.domain.todo.dto.TodoPredicate;
 import com.nzzi.guide.todo.domain.todo.dto.TodoResponse;
 import com.nzzi.guide.todo.domain.todo.exception.TodoNotFoundException;
-import com.nzzi.guide.todo.domain.todo.dao.jpa.TodoRepository;
 import com.nzzi.guide.todo.domain.todo.model.Todo;
+import com.nzzi.guide.todo.domain.todo.service.TodoQueryService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,23 +13,30 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class TodoFindService {
+public class TodoQueryServiceImpl implements TodoQueryService {
 
     private TodoRepository todoRepository;
 
+    @Override
     public TodoResponse findTodo(Long id) {
-        Todo todo = todoRepository.findById(id)
-                .orElseThrow(() -> new TodoNotFoundException("todo"));
+        Todo todo = findById(id);
         return TodoResponse.of(todo);
     }
 
+    @Override
     public Page<TodoResponse> findTodos(Pageable pageable) {
         Page<Todo> todos = todoRepository.findAll(pageable);
-        return TodoResponse.of(todos);
+        return todos.map(TodoResponse::of);
     }
 
+    @Override
     public Page<TodoResponse> searchByContents(TodoPredicate predicate, Pageable pageable) {
         Page<Todo> todos = todoRepository.searchByContents(predicate, pageable);
-        return TodoResponse.of(todos);
+        return todos.map(TodoResponse::of);
+    }
+
+    private Todo findById(Long id) {
+        return todoRepository.findById(id)
+                .orElseThrow(() -> new TodoNotFoundException(id.toString()));
     }
 }
