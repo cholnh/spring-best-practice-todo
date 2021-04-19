74b74495f838182d316a27715b6fad5d6abba52d
Nzzi <cholnh1@naver.com>
2021-04-20 04:28:25 +0900

    edit exception constructor

--- src/main/java/com/nzzi/guide/todo/domain/todo/exception/TodoNotFoundException.java
+++ src/main/java/com/nzzi/guide/todo/domain/todo/exception/TodoNotFoundException.java
@@ -4,6 +4,10 @@ import com.nzzi.guide.todo.global.error.exception.EntityNotFoundException;
 
 public class TodoNotFoundException extends EntityNotFoundException {
 
+    public TodoNotFoundException() {
+        super("not found");
+    }
+
     public TodoNotFoundException(String target) {
         super(target + " is not found");
     }
