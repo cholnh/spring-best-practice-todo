f1691171e6f294804a99edcd00f8f98edbb6b03d
Nzzi <cholnh1@naver.com>
2021-04-19 23:10:47 +0900

    add mapper config

---
+++ src/main/java/com/nzzi/guide/todo/global/configuration/mapper/MapperConfig.java
@@ -0,0 +1,12 @@
+//package com.nzzi.guide.todo.global.configuration.mapper;
+//
+//import org.mapstruct.MapperConfig;
+//import org.mapstruct.Mapping;
+//import org.mapstruct.ReportingPolicy;
+//
+//@MapperConfig(
+//        componentModel = "spring",
+//        unmappedTargetPolicy = ReportingPolicy.ERROR
+//)
+//public interface MapperConfig {
+//}
