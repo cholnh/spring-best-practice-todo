f091ad7e3414518817bf1df407556017b887bb0a
Nzzi <cholnh1@naver.com>
2021-04-20 19:59:30 +0900

    add todo command Service 테스트 케이스

---
+++ src/test/java/com/nzzi/guide/todo/domain/todo/service/impl/TodoCommandServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.nzzi.guide.todo.domain.todo.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoCommandServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void Todo정보를_정상적으로_생성한다() {
+
+    }
+
+    @Test
+    void Id에_해당하는_Todo정보를_정상적으로_수정한다() {
+
+    }
+
+    @Test
+    void Id에_해당하는_Todo정보를_정상적으로_삭제한다() {
+
+    }
+}
